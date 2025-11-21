edba1001cb625871d580207f52d625b994c465b9
Henry Mendez <henry95@gmail.com>
2025-11-20 21:04:37 -0500

    Added GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./app.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,7 +20,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-NQZLVNCPL3`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-NQZLVNCPL3');
+          `}
+        </Script>
+        {children}
+      </body>
     </html>
   );
 }
